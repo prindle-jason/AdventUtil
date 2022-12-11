9a20ed355dc3d0c34883dda11fb49db087470b83
Jason Prindle <prindle.jason [at] gmail.com>
2022-12-11 16:45:44 -0500

    Replaced deprecated setup.py with pyproject.toml configuration

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='adventutil',
-    version='0.0.1',
-    author='Jason Prindle',
-    author_email='prindle.jason@gmail.com',
-    description='Personal Utility Package for adventofcode.com',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/prindle-jason/AdventUtil',
-    project_urls = {
-        "Bug Tracker": "https://github.com/prindle-jason/AdventUtil/issues"
-    },
-    license='MIT',
-    packages=['adventutil'],
-    install_requires=['requests'],  
-)
+if __name__ == "__main__":
+    setup()
